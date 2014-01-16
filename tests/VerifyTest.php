2f3cd2ba86d7a4b374f85897c679b73424fd1bec
Mark <mark.github@yandex.ru>
2014-01-16 14:55:35 +0400

    test fixed

--- tests/VerifyTest.php
+++ tests/VerifyTest.php
@@ -71,10 +71,4 @@ class VerifyTest extends PHPUnit_Framework_TestCase {
         expect($errors)->hasntKey('body');
     }
 
-    public function testAliases()
-    {
-        v("hello")->equals("hello");       
-        e("hello")->equals("hello");       
-    }
-
 }
